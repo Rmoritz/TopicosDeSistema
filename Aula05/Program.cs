8e46f01ac40cb15351e04b1021de3c260482e4ce
RAFAEL MORITZ SUMBACH <rafael.sumbach@cs.up.edu.br>
2026-04-09 10:33:26 -0300

    comecado a associao de classe

--- Aula05/Program.cs
+++ Aula05/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class livro{
+    public String autor;
+    public String titulo;
+    public int ano;
+}
+class emprestimo{
+    
+    public livro livro;
+    public String dataEmprestimo;
+    public String dataDevolução;
+
+}
+
+
+class program{
+
+    static void Main(){
+        livro l1=new livro();
+        l1.autor="Davi brito";
+        l1.titulo="a volta dos que nao foram";
+        l1.ano=2067;
+        emprestimo e1=new emprestimo();
+        e1.livro=l1;
+        e1.DataEmprestimo = "09/05/2026";
+        e1.DataDevolucao = "16/05/2026";
+        
+    }
+    
+}
+
+
